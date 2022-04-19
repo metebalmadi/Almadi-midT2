71b36182bf8b064af7c5b783fff003afbf44853d
Meteb Almadi <meteb.almadi@gmail.com>
2022-04-19 11:00:53 +0300

    done2

--- app/src/main/java/com/example/almadi_midt2/Act3.java
+++ app/src/main/java/com/example/almadi_midt2/Act3.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -72,6 +73,7 @@ public class Act3 extends AppCompatActivity {
                 if (myDB.deleteData(ETID))
                 {
                     Toasty.success(getBaseContext(), "Deleted", Toast.LENGTH_SHORT, true).show();
+                    Log.d("Meteb", "Deleted");
                 }else
                 {
                     Toasty.error(getBaseContext(), "Could not Delete", Toast.LENGTH_SHORT, true).show();
